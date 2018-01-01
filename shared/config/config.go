c4538b7e5e1ad37bc59d0b9cbc8989e134bc39a8
Cube <cubeee@users.noreply.github.com>
2018-01-01 21:21:03 +0200

    Reorganize module config files

--- shared/config/config.go
+++ shared/config/config.go
@@ -5,8 +5,8 @@ import (
 )
 
 func ReadConfig(module string, env *string) error {
-	viper.SetConfigName(module + "-config." + *env)
-	viper.AddConfigPath("./resources/config/")
+	viper.SetConfigName("config." + *env)
+	viper.AddConfigPath("./resources/config/" + module + "/")
 	viper.SetConfigType("yaml")
 	return viper.ReadInConfig()
 }
